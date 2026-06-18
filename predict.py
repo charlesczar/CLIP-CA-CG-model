0dc9345fbda3fa0bc1f3a2137b913d08f46195e5
czar <charlesczarlucas@gmail.com>
2026-06-18 22:08:41 +0800

    new dataset + normalization in evaluate and predict python

--- predict.py
+++ predict.py
@@ -119,7 +119,11 @@ if __name__ == "__main__":
     image_size = config["data"]["image_size"]
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.48145466, 0.4578275, 0.40821073],
+            std=[0.26862954, 0.26130258, 0.27577711]
+        )
     ])
 
     img_dir = str(BASE_DIR / "data" / "images")
